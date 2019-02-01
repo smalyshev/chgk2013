b5a5713667e707e06ece8dc1eac6e93ace31c69e
Stanislav Malyshev <smalyshev@gmail.com>
2019-01-31 19:28:04 -0800

    update

--- chgk.php
+++ chgk.php
@@ -4,19 +4,19 @@
 </font></td></tr>
 	<tr><td colspan="2" align="center"><?= $row['gorod'] ?></td></tr>
 	<tr valign="top">
-	<td width="50%">
-	<?php 
+	<td>
+	<?php
 	$c = count($igroki);
 	$h = ($c>6?4:3);
 	for($i=0; $i<$h; $i++) {
 		if(empty($igroki[$i])) break;
-		echo $igroki[$i]."<br>";		
+		echo $igroki[$i]."<br>";
 	}?>
 	</td>
-	<td width="50%">
+	<td>
 	<?php for(; $i<$c; $i++) {
 		if(empty($igroki[$i])) break;
-		echo $igroki[$i]."<br>";		
+		echo $igroki[$i]."<br>";
 	}?>
 	</td>
 	</tr>
